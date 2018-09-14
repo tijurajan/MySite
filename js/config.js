1ec6292fe77c0602da56a0cf33f7ea3d9ced7f4c
OMNICOM\tiju.rajanabraham <tiju.rajanabraham@au.tribalworldwide.com>
2018-09-14 13:21:16 +1000

    First commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-southeast-2_W5Ffmvlh6', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '38bkr7i866ankjeualb23l6152', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
